b397fab916cf80ddc3e5bd09370f82bcebaf0b46
RbnAlexs <rubensanchezcolin@gmail.com>
2018-07-27 02:26:29 -0500

    functions update destroy

--- app/Http/Controllers/PastelesController.php
+++ app/Http/Controllers/PastelesController.php
@@ -25,4 +25,29 @@ class PastelesController extends Controller
 
         return redirect()->route('pasteles.index');
     }
+
+    public function edit($id)
+    {
+        $pastel = Pastel::find($id);
+        return view('pasteles.edit')->with('pastel',$pastel);
+    }
+
+    public function update(Request $request, $id)
+   {
+       $pastel = Pastel::find($id);
+       $pastel->nombre = $request->input('nombre');
+       $pastel->sabor  = $request->input('sabor');
+       $pastel->save();
+       return redirect()->route('pasteles.index');
+   }
+
+   // Esta es la primer opcion
+   public function destroy($id)
+   {
+       $pastel = Pastel::find($id);
+       $pastel->delete();
+       return redirect()->route('pasteles.index');
+   }
+
+
 }
